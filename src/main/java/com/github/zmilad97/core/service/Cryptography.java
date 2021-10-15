e42dbfbd5f81ed341368006c6cd064452ba5ab22
milad <zmilad97@gmail.com>
2021-10-15 11:50:00 +0330

    changed return type of Cryptography

--- src/main/java/com/github/zmilad97/core/service/Cryptography.java
+++ src/main/java/com/github/zmilad97/core/service/Cryptography.java
@@ -1,34 +1,27 @@
 package com.github.zmilad97.core.service;
 
-import java.math.BigInteger;
+import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Cryptography {
-      private final MessageDigest md;
+    private final MessageDigest md;
 
-      public Cryptography() {
+    public Cryptography() {
         try {
-          md = MessageDigest.getInstance("SHA-256");
+            md = MessageDigest.getInstance("SHA-256");
         } catch (NoSuchAlgorithmException e) {
-          throw new IllegalStateException(e);
+            throw new IllegalStateException(e);
         }
-      }
+    }
 
-      public byte[] getSha(String input) {
+    public byte[] getSha(String input) {
         return md.digest(input.getBytes(StandardCharsets.UTF_8));
-      }
+    }
 
     public String toHexString(byte[] hash) {
+        return DatatypeConverter.printHexBinary(hash);
 
-        BigInteger number = new BigInteger(1, hash);
-
-        StringBuilder hexString = new StringBuilder(number.toString(16));
-
-        while (hexString.length() < 32)
-            hexString.insert(0, '0');
-
-        return hexString.toString();
     }
 }
