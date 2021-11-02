7d59c07ab4cef38f5b1f4bdd8bf7534e57ca4e35
milad <zmilad97@gmail.com>
2021-11-02 15:25:27 +0330

    added InvalidBlockException

---
+++ src/main/java/com/github/zmilad97/core/exceptions/InvalidBlockException.java
@@ -0,0 +1,7 @@
+package com.github.zmilad97.core.exceptions;
+
+public class InvalidBlockException extends Exception {
+    public InvalidBlockException() {
+        super("Block is invalid");
+    }
+}
